13f69b9297114409d449335343ce1a9899a63d21
Sylex1002 <safidiniainasylex@gmail.com>
2025-05-17 21:58:06 +0300

    fix fix

--- frontend/src/components/Dashboard/components/userList/UserList.jsx
+++ frontend/src/components/Dashboard/components/userList/UserList.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import Filters from './Filters/Filters';
 import SearchInput from '../../common/SearchInput/SearchInput';
 import ListTable from '../../common/ListTable/ListTable';
-import { Outlet } from 'react-router-dom';
 
 const ListUser = () => {
     return (
-        <div className='py-6 px-8 bg-soft-white rounded-[3rem]'>
+        <div className='h-screen py-6 px-8 bg-soft-white rounded-[3rem] flex flex-col'>
             <div className='flex justify-between'>
                 <Filters />
                 <SearchInput />
             </div>
-            <div className='pt-5'>
+            <div className='pt-5 flex-grow'>
                 <ListTable />
             </div>
         </div>
