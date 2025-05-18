4e2e0522fe9d7f946c29ae1de3292781fbeff9e7
Niaina42 <niainachristopher4@gmail.com>
2025-05-18 10:05:01 +0300

    fix

--- frontend/src/hooks/useHttps.js
+++ frontend/src/hooks/useHttps.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // export const rootApiUrl = "https://api.techtitan.madagascar.webcup.hodi.host/api" // "http://localhost:9002/api"; // 
-export const rootApiUrl = "http://localhost:9002/api" // "http://localhost:9002/api"; // 
+export const rootApiUrl = "https://api.techtitan.madagascar.webcup.hodi.host/api" // "http://localhost:9002/api"; // 
 export const aiUrl = "https://royal-tilapia.com/face";
 export const imgUrl = "https://api.techtitan.madagascar.webcup.hodi.host/public" // "http://localhost:9002/public"; // 
-export const BaseUrl = "http://localhost:9002" // "https://api.techtitan.madagascar.webcup.hodi.host" 
+export const BaseUrl = "https://api.techtitan.madagascar.webcup.hodi.host" // "https://api.techtitan.madagascar.webcup.hodi.host" 
 
 const useHttps = () => {
   const http = axios.create({
